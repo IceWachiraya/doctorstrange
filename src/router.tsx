22d0f45fde5896dbe2aef7d95a18a386caa2be92
Wachiice <wachiraya.ph@kkumail.com>
2024-06-07 16:14:31 +0700

    Authen

--- src/router.tsx
+++ src/router.tsx
@@ -1,37 +1,37 @@
-import { LoaderFunctionArgs, createBrowserRouter, redirect } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import TodoApp from "./TodoApp";
 import HomePage from "./components/HomePage";
 import App from "./App";
 interface AuthProvider {
-    isAuthenticated: boolean;
-    username: null | string;
-    signin(username: string): Promise<void>;
-    signout(): Promise<void>;
+  isAuthenticated: boolean;
+  username: null | string;
+  signin(username: string): Promise<void>;
+  signout(): Promise<void>;
+}
+
+/**
+ * This represents some generic auth provider API, like Firebase.
+ */
+export const fakeAuthProvider: AuthProvider = {
+  isAuthenticated: false,
+  username: null,
+  async signin(username: string) {
+    await new Promise((r) => setTimeout(r, 500)); // fake delay
+    fakeAuthProvider.isAuthenticated = true;
+    fakeAuthProvider.username = username;
+  },
+  async signout() {
+    await new Promise((r) => setTimeout(r, 500)); // fake delay
+    fakeAuthProvider.isAuthenticated = false;
+    fakeAuthProvider.username = "";
+  },
+};
+
+function protectedLoader() {
+  if (!fakeAuthProvider.isAuthenticated) {
+    return redirect("/");
   }
-  
-  /**
-   * This represents some generic auth provider API, like Firebase.
-   */
-  export const fakeAuthProvider: AuthProvider = {
-    isAuthenticated: false,
-    username: null,
-    async signin(username: string) {
-      await new Promise((r) => setTimeout(r, 500)); // fake delay
-      fakeAuthProvider.isAuthenticated = true;
-      fakeAuthProvider.username = username;
-    },
-    async signout() {
-      await new Promise((r) => setTimeout(r, 500)); // fake delay
-      fakeAuthProvider.isAuthenticated = false;
-      fakeAuthProvider.username = "";
-    },
-  };
-  
-function protectedLoader({request}:LoaderFunctionArgs){
-    if(!fakeAuthProvider.isAuthenticated){
-        return redirect("/");
-    }
-    return null;
+  return null;
 }
 const router = createBrowserRouter([
   {
@@ -42,15 +42,15 @@ const router = createBrowserRouter([
         path: "/",
         element: <HomePage />,
       },
-    //   {
-    //     path: "/todo",
-    //     element: <TodoApp />,
-    //   },
+      //   {
+      //     path: "/todo",
+      //     element: <TodoApp />,
+      //   },
       {
-        path:"/todo",
-        loader:protectedLoader,
-        Component:TodoApp,
-      }
+        path: "/todo",
+        loader: protectedLoader,
+        Component: TodoApp,
+      },
     ],
   },
 ]);
