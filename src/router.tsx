9cb45b0704009dbcf51f7ce124e474b5053474c9
Wachiice <wachiraya.ph@kkumail.com>
2024-06-07 16:08:25 +0700

    Authen

--- src/router.tsx
+++ src/router.tsx
@@ -1,23 +1,58 @@
-import { createBrowserRouter } from "react-router-dom";
+import { LoaderFunctionArgs, createBrowserRouter, redirect } from "react-router-dom";
 import TodoApp from "./TodoApp";
 import HomePage from "./components/HomePage";
 import App from "./App";
-
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      children:[
-        {
-            path:"/",
-            element:<HomePage/>,
-        },
-        {
-            path:"/todo",
-            element:<TodoApp/>
-        }
-      ]
+interface AuthProvider {
+    isAuthenticated: boolean;
+    username: null | string;
+    signin(username: string): Promise<void>;
+    signout(): Promise<void>;
+  }
+  
+  /**
+   * This represents some generic auth provider API, like Firebase.
+   */
+  export const fakeAuthProvider: AuthProvider = {
+    isAuthenticated: false,
+    username: null,
+    async signin(username: string) {
+      await new Promise((r) => setTimeout(r, 500)); // fake delay
+      fakeAuthProvider.isAuthenticated = true;
+      fakeAuthProvider.username = username;
     },
+    async signout() {
+      await new Promise((r) => setTimeout(r, 500)); // fake delay
+      fakeAuthProvider.isAuthenticated = false;
+      fakeAuthProvider.username = "";
+    },
+  };
+  
+function protectedLoader({request}:LoaderFunctionArgs){
+    if(!fakeAuthProvider.isAuthenticated){
+        return redirect("/");
+    }
+    return null;
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    //   {
+    //     path: "/todo",
+    //     element: <TodoApp />,
+    //   },
+      {
+        path:"/todo",
+        loader:protectedLoader,
+        Component:TodoApp,
+      }
+    ],
+  },
+]);
 
-  ]);
-  export default router;
+export default router;
